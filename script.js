066a60ee775ec63b0734fc57bfc7aebf1909fdb6
al-shahzeb <99374818+al-shahzeb@users.noreply.github.com>
2023-06-24 20:16:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+function prms() {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!")
+		},1000)
+	})
+}
+
+prms().then((data)=>{
+	document.getElementById("output").innerText = data;
+})
+
+
+
+
+
+
